248d4454c92e75159b9776a9285216117848dcb1
Gabriel Rossetti <rossetti.gabriel@gmail.com>
2012-07-15 19:27:48 +0200

    Merged Rich Sedman's extention to do the same thing but in the overview search.

--- DashClickFix@evotex.ch/extension.js
+++ DashClickFix@evotex.ch/extension.js
@@ -4,17 +4,22 @@
  *       running and to switch to the current instance if it is already 
  *       running. This extension changes that to instead of switching to 
  *       it if it is already running it always launches a new instance.
+ *       It also always open a new window when launching applications 
+ *       from the Overview Search when typing the application name and 
+ *       pressing ENTER (rather than using the mouse).
  * 
  * Author: Gabriel Rossetti
- * Date: 2012-05-03
- * Version: 1.1
+ * Author: Rich Sedman
+ * Date: 2012-07-15
+ * Version: 2.0
  */
 const Main = imports.ui.main;
 const AppDisplay = imports.ui.appDisplay;
+const Params = imports.misc.params;
 
-
-var _original = null;
-
+var _onActivateOriginal = null;
+var _activateResultOriginal = null;
+	
 /**
  * The new version of the function, this always lanches a new version of 
  * the app regardless of if it is already running or not.
@@ -34,17 +39,35 @@ function _onActivate(event) {
 }
 
 /**
+ * The new version of the function, this always lanches a new version of 
+ * the app from the Overviwe Search typing the application name and 
+ * pressing ENTER; regardless of if it is already running or not.
+ * 
+ * @param app the app that will be lanched
+ * @param params the app's launch parameters
+ */
+function _activateResult(app, params) {
+
+  params = Params.parse(params, { workspace: -1, timestamp: 0 });
+  app.open_new_window(params.workspace);
+}
+
+/**
  * Initialize the extension
  */
 function init() {
-  _original = AppDisplay.AppWellIcon.prototype._onActivate;
+
+  _onActivateOriginal = AppDisplay.AppWellIcon.prototype._onActivate;
+  _activateResultOriginal = AppDisplay.AppSearchProvider.prototype.activateResult;
 }
 
 /**
  * Enable the extension
  */
 function enable() {
+
   AppDisplay.AppWellIcon.prototype._onActivate = _onActivate;
+  AppDisplay.AppSearchProvider.prototype.activateResult = _activateResult;
 }
 
 /**
@@ -52,5 +75,6 @@ function enable() {
  */
 function disable() {
   
-  AppDisplay.AppWellIcon.prototype._onActivate = _original;
+  AppDisplay.AppWellIcon.prototype._onActivate = _onActivateOriginal;
+  AppDisplay.AppSearchProvider.prototype.activateResult = _activateResultOriginal;
 }
